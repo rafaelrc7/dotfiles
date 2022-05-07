64859fa87dd10c88afd4f4ac16fb5f576581d6d9
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-07 01:18:32 -0300

    Make awesome use lua 5.3

--- hosts/spitfire/default.nix
+++ hosts/spitfire/default.nix
@@ -70,10 +70,10 @@
 
     windowManager.awesome = {
       enable = true;
-      package = pkgs.awesome.overrideAttrs (old: {
+      package = (pkgs.awesome.overrideAttrs (old: {
         version = "git";
         src = inputs.awesome-git;
-      });
+      })).override { lua = pkgs.lua5_3; };
     };
 
     layout = "us";
