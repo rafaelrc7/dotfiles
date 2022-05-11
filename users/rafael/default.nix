e24b48a41887c62d75d7ae1bbaf54bf1bc048a61
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-11 20:28:12 -0300

    Change kitty opacity

--- users/rafael/default.nix
+++ users/rafael/default.nix
@@ -105,7 +105,7 @@
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
-      background_opacity = "0.95";
+      background_opacity = "0.98";
       enable_audio_bell = false;
       scrollback_pager_history_size = 2048;
       mouse_map = "left click ungrabbed no-op";
