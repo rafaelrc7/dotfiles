11cab3e1115d2475e5a17315284808d07cb5d319
rafaelrc7 <contact@rafaelrc.com>
2022-08-03 03:16:06 -0300

    Fix awesome mainline build

--- modules/nixos/awesomewm.nix
+++ modules/nixos/awesomewm.nix
@@ -5,6 +5,7 @@
       package = (pkgs.awesome.overrideAttrs (old: {
         version = "git";
         src = inputs.awesome-git;
+        patches = [ ];
       })).override { lua = pkgs.lua5_3; };
     };
 
