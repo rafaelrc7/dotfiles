04f280476a30fc77f007eae17b0bd2cd38ed843e
rafaelrc7 <contact@rafaelrc.com>
2022-09-02 23:37:32 -0300

    Add general database

--- hosts/vulcan/default.nix
+++ hosts/vulcan/default.nix
@@ -8,6 +8,7 @@
 
   environment = {
     systemPackages = with pkgs; [
+      dbeaver
       libva-utils
       lutris
       glxinfo
@@ -74,6 +75,15 @@
         };
       };
     };
+  };
+
+  services.postgresql = {
+    enable = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all peer
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
+    '';
   };
 
   services = {
