d5f3d078a4091041203540f99fc20468b2b1e09a
rafaelrc7 <contact@rafaelrc.com>
2022-12-23 03:05:53 -0300

    Fix nix gc time

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -19,7 +19,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 1w";
+      options = "--delete-older-than 7d";
     };
 
     registry.nixpkgs.flake = inputs.nixpkgs;
