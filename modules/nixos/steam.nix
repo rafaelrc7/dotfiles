62c4307d41ea8868472a5296349f4829d8324713
rafaelrc7 <contact@rafaelrc.com>
2023-08-08 22:16:27 -0300

    Improve steam config to work with wayland
    Still, only flatpak works properly :(

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -1,10 +1,38 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: let
+  extraLibraries = pkgs: with pkgs; [
+    jdk
+  ];
+
+  steamWithExtraLibraries = pkgs.steam.override {
+    extraPkgs = extraLibraries;
+  };
+  wrapSteam = steam: pkgs.writeScriptBin "steam" ''
+    unset SDL_VIDEODRIVER
+    unset QT_QPA_PLATFORM
+    unset QT_WAYLAND_DISABLE_WINDOWDECORATION
+    unset MOZ_ENABLE_WAYLAND
+    unset CLUTTER_BACKEND
+    unset XDG_SESSION_TYPE
+
+    exec ${steam}/bin/steam
+  '';
+in {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+    package = lib.makeOverridable (o:
+      let overridenSteam = steamWithExtraLibraries.override o;
+      in (pkgs.symlinkJoin {
+        name = "steam";
+        paths = [
+          (wrapSteam overridenSteam)
+          overridenSteam
+        ];
+      }).overrideAttrs { run = overridenSteam.run; }) { };
   };
 
+  hardware.steam-hardware.enable = true;
   programs.java.enable = true;
 }
 
