a49e6f209f76c79759e0c5e5f5f410085e2c46ee
rafaelrc7 <contact@rafaelrc.com>
2023-09-07 13:50:39 -0300

    Update theme

--- users/rafael/gui-theme.nix
+++ users/rafael/gui-theme.nix
@@ -71,13 +71,15 @@
 
   qt = {
     enable = true;
-    platformTheme = "gtk";
-    style.name = "kvantum"; # set QT_STYLE_OVERRIDE
+    platformTheme = "qtct";
+    # style.name = "kvantum"; # set QT_STYLE_OVERRIDE
   };
 
   home.packages = with pkgs; [
     libsForQt5.qtstyleplugins
-    libsForQt5.qtstyleplugin-kvantum
+    libsForQt5.qtstyleplugin-kvantum  qt6Packages.qtstyleplugin-kvantum
+    libsForQt5.qt5.qtwayland          qt6.qtwayland
+    libsForQt5.qt5ct                  qt6Packages.qt6ct
   ];
 
   xdg.configFile = {
@@ -90,6 +92,71 @@
     "Kvantum/Dracula-Solid".source = "${pkgs.dracula-theme}/share/Kvantum/Dracula-Solid";
     "Kvantum/Dracula-purple".source = "${pkgs.dracula-theme}/share/Kvantum/Dracula-purple";
     "Kvantum/Dracula-purple-solid".source = "${pkgs.dracula-theme}/share/Kvantum/Dracula-purple-solid";
+
+    "qt5ct/qt5ct.conf".text = ''
+      [Appearance]
+      custom_palette=false
+      icon_theme=Dracula
+      standard_dialogs=default
+      style=kvantum
+
+      [Fonts]
+      fixed="DejaVu Sans,12,-1,5,50,0,0,0,0,0"
+      general="DejaVu Sans,12,-1,5,50,0,0,0,0,0"
+
+      [Interface]
+      activate_item_on_single_click=1
+      buttonbox_layout=0
+      cursor_flash_time=1000
+      dialog_buttons_have_icons=1
+      double_click_interval=400
+      gui_effects=@Invalid()
+      keyboard_scheme=2
+      menus_have_icons=true
+      show_shortcuts_in_context_menus=true
+      stylesheets=@Invalid()
+      toolbutton_style=4
+      underline_shortcut=1
+      wheel_scroll_lines=3
+
+      [SettingsWindow]
+      geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\0\0\0\0\0\0\0\x4\xf9\0\0\x4\x1a\0\0\0\0\0\0\0\0\0\0\x2\xde\0\0\x2\xd9\0\0\0\0\x2\0\0\0\n\0\0\0\0\0\0\0\0\0\0\0\x4\xf9\0\0\x4\x1a)
+
+      [Troubleshooting]
+      force_raster_widgets=1
+      ignored_applications=@Invalid()
+    '';
+
+    "qt6ct/qt6ct.conf".text = ''
+      [Appearance]
+      custom_palette=false
+      icon_theme=Dracula
+      standard_dialogs=default
+      style=kvantum
+
+      [Fonts]
+      fixed="DejaVu Sans,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
+      general="DejaVu Sans,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
+
+      [Interface]
+      activate_item_on_single_click=1
+      buttonbox_layout=0
+      cursor_flash_time=1000
+      dialog_buttons_have_icons=1
+      double_click_interval=400
+      gui_effects=@Invalid()
+      keyboard_scheme=2
+      menus_have_icons=true
+      show_shortcuts_in_context_menus=true
+      stylesheets=@Invalid()
+      toolbutton_style=4
+      underline_shortcut=1
+      wheel_scroll_lines=3
+
+      [Troubleshooting]
+      force_raster_widgets=1
+      ignored_applications=@Invalid()
+    '';
   };
 }
 
