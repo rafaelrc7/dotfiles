7999f6500f070f82b15666f2f95f302c730df1e8
rafaelrc7 <contact@rafaelrc.com>
2024-01-10 15:52:52 -0300

    Improve printer settings

--- hosts/spitfire/default.nix
+++ hosts/spitfire/default.nix
@@ -109,6 +109,7 @@
     avahi = {
       enable = true;
       nssmdns4 = true;
+      openFirewall = true;
       publish.enable = true;
       publish.addresses = true;
       publish.userServices= true;
@@ -117,7 +118,28 @@
 
   hardware.sane = {
     enable = true;
-    extraBackends = [ pkgs.epson-escpr ];
+    extraBackends = [ pkgs.sane-airscan pkgs.epkowa ];
+    disabledDefaultBackends = [ "v4l" "epson2" ];
+  };
+  services.ipp-usb.enable = true;
+
+  hardware.printers = {
+    ensureDefaultPrinter = "L355-Series";
+    ensurePrinters = [
+      {
+        name = "L355-Series";
+        location = "office";
+        deviceUri = "usb://EPSON/L355%20Series?serial=53335A4B3039313191&interface=1";
+        model = "epson-inkjet-printer-201207w/ppds/EPSON_L355.ppd";
+        ppdOptions = {
+          MediaType = "PLAIN";
+          PrintQuality = "High";
+          PageSize = "A4";
+          OutputPaper = "A4";
+          Color = "Grayscale";
+        };
+      }
+    ];
   };
 
   programs.dconf.enable = true;
