b5efec1cc82dba456dde11d211cae29df43a2bb6
rafaelrc7 <contact@rafaelrc.com>
2024-02-03 03:46:28 -0300

    Fix user pass settings

--- users/rafael/default.nix
+++ users/rafael/default.nix
@@ -72,6 +72,11 @@
     ];
   };
 
+  programs.password-store.settings = {
+      PASSWORD_STORE_KEY = "081F97AC49F2CA9548DB08E7091BB8A361C7B4EB";
+      PASSWORD_STORE_DIR = "$HOME/.password-store";
+  };
+
   services.keybase.enable = true;
 
   systemd.user.startServices = "sd-switch";
