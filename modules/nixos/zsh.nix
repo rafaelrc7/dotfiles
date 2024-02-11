e9371d2408af6acb7f3d4502b80c0a190c5d097c
rafaelrc7 <contact@rafaelrc.com>
2024-02-11 20:39:57 -0300

    Remove unused alias

--- modules/nixos/zsh.nix
+++ modules/nixos/zsh.nix
@@ -25,7 +25,6 @@
       ip = "ip --color=auto";
       ls = "ls --color=auto";
       sudo = "sudo "; # Makes commands after sudo keep colour
-      lutris = "WEBKIT_DISABLE_COMPOSITING_MODE=1 lutris";
     };
 
     shellInit =
