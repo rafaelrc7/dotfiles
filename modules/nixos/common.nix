6e3838d3dda30fbb26ac1978f68eb75c40c93b5e
rafaelrc7 <contact@rafaelrc.com>
2024-03-09 20:46:33 -0300

    Automatically run appimages with appimage-run

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -36,5 +36,14 @@
     layout = "us";
     variant = "intl";
   };
+
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
 }
 
