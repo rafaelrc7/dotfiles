b823d582d0ba6ec61c5c0815ea28ccb68691e62a
rafaelrc7 <contact@rafaelrc.com>
2024-07-12 02:24:04 -0300

    Update modorganizer2-linux-installer

--- pkgs/modorganizer2-linux-installer/default.nix
+++ pkgs/modorganizer2-linux-installer/default.nix
@@ -9,11 +9,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "modorganizer2-linux-installer";
-  version = "4.6.1";
+  version = "5.0.3";
 
   src = fetchurl {
     url = "https://github.com/rockerbacon/modorganizer2-linux-installer/releases/download/${version}/mo2installer-${version}.tar.gz";
-    sha256 = "sha256-17nZA4vP791Q3Mky5mViEvBbibE04eBdrR7G5D269vI=";
+    sha256 = "sha256-C79xZvnAc4hJ0Io32JVye9HBecNtZByd7eJIH+z/uV4=";
   };
 
   sourceRoot = ".";
