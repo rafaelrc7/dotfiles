c1dfaba00c2591efadad631ecac7e7cb3ff31e00
rafaelrc7 <contact@rafaelrc.com>
2024-08-11 12:48:07 -0300

    Disable monero pruning

--- modules/nixos/monero.nix
+++ modules/nixos/monero.nix
@@ -2,8 +2,6 @@
   services.monero = {
     enable = true;
     extraConfig = ''
-      prune-blockchain=1
-      sync-pruned-blocks=1
       db-sync-mode=safe
       confirm-external-bind=1
     '';
