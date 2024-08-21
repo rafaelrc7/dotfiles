9238435983e3f178154b15b00ba7f28c9793be65
rafaelrc7 <contact@rafaelrc.com>
2024-08-21 17:06:12 -0300

    Enable podman socket and autoPrune

--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -2,6 +2,8 @@
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
+    dockerSocket.enable = true;
+    autoPrune.enable = true;
     defaultNetwork.settings.dns_enabled = true;
   };
 
