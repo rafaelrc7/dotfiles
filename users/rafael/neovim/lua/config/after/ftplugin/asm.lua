e93dcacdf0426dd3bcd45383a4ed3fe5af38d72d
rafaelrc7 <contact@rafaelrc.com>
2024-09-25 17:16:47 -0300

    nvim: lsp takes care for formatting now

--- users/rafael/neovim/lua/config/after/ftplugin/asm.lua
+++ users/rafael/neovim/lua/config/after/ftplugin/asm.lua
@@ -1,2 +1,4 @@
 vim.opt_local.tabstop = 8
 vim.opt_local.shiftwidth = 8
+vim.opt_local.softtabstop = 8
+vim.opt_local.expandtab = false
--- users/rafael/neovim/lua/config/after/ftplugin/rust.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-	buffer = 0,
-	command = [[silent !cargo fmt]],
-	group = vim.api.nvim_create_augroup("rustfmt", { clear = true }),
-})
