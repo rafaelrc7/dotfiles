afc3c3d14dd3990870497d62d6ec11b6e767a434
rafaelrc7 <contact@rafaelrc.com>
2024-09-25 12:32:29 -0300

    Remove now unnecessary neovim plugin overrides

--- overlays/neovim-plugins.nix
+++ overlays/neovim-plugins.nix
@@ -5,19 +5,10 @@ let
     name = "remote-nvim";
     src = inputs.remote-nvim;
   };
-
-  lsp-progress-nvim = prev.vimUtils.buildVimPlugin {
-    name = "lsp-progress.nvim";
-    src = inputs.lsp-progress-nvim;
-  };
-
-  trouble-nvim = prev.vimPlugins.trouble-nvim.overrideAttrs (old: {
-    src = inputs.trouble-nvim;
-  });
 in
 {
   vimPlugins = prev.vimPlugins // {
-    inherit remote-nvim lsp-progress-nvim trouble-nvim;
+    inherit remote-nvim;
   };
 }
 
