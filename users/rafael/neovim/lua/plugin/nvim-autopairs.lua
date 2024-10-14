9bf5b448bba317e78bfa98c44886d35fb7ecf1ff
rafaelrc7 <contact@rafaelrc.com>
2024-10-13 23:50:31 -0300

    neovim: improve single quote autopairs settings

--- users/rafael/neovim/lua/plugin/nvim-autopairs.lua
+++ users/rafael/neovim/lua/plugin/nvim-autopairs.lua
@@ -1,12 +1,31 @@
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = require("cmp")
+local npairs = require "nvim-autopairs"
+local Rule = require "nvim-autopairs.rule"
+local cond = require "nvim-autopairs.conds"
+local ts_conds = require "nvim-autopairs.ts-conds"
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp = require "cmp"
 
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-require("nvim-autopairs").setup({
+npairs.setup {
 	check_ts = true,
 	enable_check_bracket_line = false,
 	fast_wrap = {},
-})
+}
 
-require("nvim-autopairs.ts-conds")
+npairs.get_rules "'"[1].not_filetypes = { "scheme", "lisp" }
+require "nvim-autopairs".get_rules "'"[1]:with_pair(function(opts)
+	local prev = string.sub(opts.text, 1, opts.col)
+	print(string.match(prev, "%w+\'*$"))
+	return not string.match(prev, "%w+\'*$")
+end)
+
+npairs.add_rule(Rule("<", ">", {
+	"-html",
+	"-javascriptreact",
+	"-typescriptreact",
+}):with_pair(
+	cond.before_regex("%a+:?:?$", 3)
+):with_move(function(opts)
+	return opts.char == ">"
+end))
