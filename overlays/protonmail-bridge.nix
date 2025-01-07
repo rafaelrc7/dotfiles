e21e8741b6a7ff49e7cbf81eaa58492449b874f4
rafaelrc7 <contact@rafaelrc.com>
2025-01-07 20:15:00 -0300

    Update protonmail-bridge

--- overlays/protonmail-bridge.nix
+++ overlays/protonmail-bridge.nix
@@ -1,9 +1,9 @@
 { inputs, ... }:
 final: prev: {
   protonmail-bridge = prev.protonmail-bridge.overrideAttrs (old: {
-    version = "3.14.0";
+    version = "3.16.0";
     src = inputs.proton-bridge;
-    vendorHash = "sha256-I/OFpEa3aB+qDBS/sbX5WOgrlSyR7aZaQYrsaSVNAAk=";
+    vendorHash = "sha256-RKAkdCTkUcUKO+eXvn/Sh52Un4DzvgUlF19MiGm/K0c=";
   });
 }
 
