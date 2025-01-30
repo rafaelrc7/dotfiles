2cc39643f6e488fac36d18ad2fa207461a273cdd
rafaelrc7 <contact@rafaelrc.com>
2025-01-30 00:00:50 -0300

    Fix sdrpp

---
+++ overlays/sdrpp.nix
@@ -0,0 +1,15 @@
+{ ... }:
+final: prev:
+{
+  sdrpp = final.symlinkJoin {
+    pname = prev.sdrpp.pname;
+    version = prev.sdrpp.version;
+    paths = [ prev.sdrpp ];
+    buildInputs = [ final.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/sdrpp \
+        --set PIPEWIRE_NOJACK 1
+    '';
+  };
+}
+
