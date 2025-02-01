4f34f7028c2126a432167b856be752a49658d7f9
rafaelrc7 <contact@rafaelrc.com>
2025-02-01 18:37:30 -0300

    pkgs: Chatbox 1.9.5

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
   krisp-patcher = pkgs.callPackage ./krisp-patcher { };
   modorganizer2-linux-installer = pkgs.callPackage ./modorganizer2-linux-installer { };
   vim-spell-dict = pkgs.callPackage ./vim-spell { };
+  chatbox = pkgs.callPackage ./chatbox { };
 }
 
