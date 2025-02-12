1aa0de76795f2ca8ebc6ea87f7e4ff10dc1c9b9a
rafaelrc7 <contact@rafaelrc.com>
2025-02-11 21:30:17 -0300

    Fix systemd-oomd

--- modules/nixos/systemd-oomd.nix
+++ modules/nixos/systemd-oomd.nix
@@ -5,5 +5,50 @@
     enableUserSlices = true;
     enableSystemSlice = true;
     enableRootSlice = true;
+    extraConfig = {
+      DefaultMemoryPressureDurationSec = "20";
+      SwapUsedLimitPercent = "90%";
+    };
+  };
+
+  systemd.extraConfig = ''
+    DefaultCPUAccounting=yes
+    DefaultIOAccounting=yes
+    DefaultMemoryAccounting=yes
+    DefaultTasksAccounting=yes
+  '';
+
+  # Services
+  systemd.services."user@".serviceConfig = {
+    ManagedOOMMemoryPressure = "kill";
+    ManagedOOMMemoryPressureLimit = "40%";
+  };
+
+  # Slices
+  ## Root
+  systemd.slices."-".sliceConfig = {
+    ManagedOOMSwap = "kill";
+    ManagedOOMMemoryPressure = "kill";
+    ManagedOOMMemoryPressureLimit = "80%";
+  };
+
+  ## System
+  systemd.slices."system".sliceConfig = {
+    ManagedOOMSwap = "kill";
+    ManagedOOMMemoryPressure = "kill";
+    ManagedOOMMemoryPressureLimit = "80%";
+  };
+
+  ## User
+  systemd.slices.user.sliceConfig = {
+    ManagedOOMSwap = "kill";
+    ManagedOOMMemoryPressure = "kill";
+    ManagedOOMMemoryPressureLimit = "40%";
+  };
+
+  systemd.slices."user-".sliceConfig = {
+    ManagedOOMSwap = "kill";
+    ManagedOOMMemoryPressure = "kill";
+    ManagedOOMMemoryPressureLimit = "40%";
   };
 }
