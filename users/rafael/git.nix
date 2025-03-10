1fa297ced31e6e59c2d3047e8b7d05b0c775b291
rafaelrc7 <contact@rafaelrc.com>
2025-03-10 16:56:39 -0300

    Update git config

--- users/rafael/git.nix
+++ users/rafael/git.nix
@@ -6,20 +6,50 @@
     userEmail = "contact@rafaelrc.com";
     delta.enable = true;
     extraConfig = {
+      branch.sort = "-committerdate";
+      column.ui = "auto";
+      commit.verbose = true;
+      core = {
+        fsmonitor = true;
+        untrackedcache = true;
+      };
       delta.navigate = true;
-      diff.algorithm = "histogram";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        all = true;
+        pruneTags = true;
+        prune = true;
+      };
+      help.autocorrect = "prompt";
       init.defaultBranch = "master";
       merge.conflictStyle = "zdiff3";
       pull.rebase = true;
-      rebase.autosquash = true;
-      rerere.enabled = true;
+      push = {
+        autoSetupRemote = true;
+        default = "simple";
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        autoupdate = true;
+        enabled = true;
+      };
+      tag.sort = "version:refname";
       user.editor = "vim";
     };
     ignores = [
       "*~"
       "*.swp"
       ".idea"
-      ".vscode"
       ".direnv"
     ];
     signing = {
