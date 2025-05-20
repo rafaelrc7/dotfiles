218e9034c263a0a7dd294f885e259754cf81cf67
rafaelrc7 <contact@rafaelrc.com>
2025-05-20 18:44:46 -0300

    Update stable to 25.05

--- flake.nix
+++ flake.nix
@@ -79,7 +79,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs";
 
