46b17bd91196edfe9758096247cc164d40960b1c
rafaelrc7 <contact@rafaelrc.com>
2026-03-18 23:00:47 -0300

    Fix postgresql auth

--- modules/nixos/postgresql.nix
+++ modules/nixos/postgresql.nix
@@ -3,7 +3,7 @@
   services.postgresql = {
     enable = true;
     authentication = lib.mkOverride 10 ''
-      local all all peer
+      local all all trust
       host all all 127.0.0.1/32 trust
       host all all ::1/128 trust
     '';
