d4ab5a6ec80e322375c42eb91629a8ef0c35af37
rafaelrc7 <contact@rafaelrc.com>
2026-04-25 02:41:08 -0300

    nvim: Remove <esc> bind

--- users/rafael/neovim/lua/maps.lua
+++ users/rafael/neovim/lua/maps.lua
@@ -4,8 +4,5 @@ vim.keymap.set("n", ";", "", { noremap = true })
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = ";"
 
--- Esc removes highlight
-vim.keymap.set("n", "<Esc>", ":nohl<CR>", { silent = true, desc = "Remove highlights" })
-
 -- Toggle spellcheck
 vim.keymap.set("", "<leader>s", ":setlocal spell!<CR>", { silent = true, noremap = true, desc = "Toggle [s]pellcheck" })
