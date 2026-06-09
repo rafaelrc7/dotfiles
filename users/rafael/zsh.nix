5f203720ebcaa18ecf58d65c623d717c019b76e9
rafaelrc7 <contact@rafaelrc.com>
2026-06-09 14:28:05 -0300

    Update zsh config

--- users/rafael/zsh.nix
+++ users/rafael/zsh.nix
@@ -15,10 +15,16 @@
     enable = true;
     dotDir = "${config.xdg.configHome}/zsh";
 
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
+    autosuggestion.strategy = [
+      "completion"
+      "match_prev_cmd"
+      "history"
+    ];
+
+    enableCompletion = true;
     enableVteIntegration = true;
+    syntaxHighlighting.enable = true;
 
     defaultKeymap = "viins";
 
@@ -26,10 +32,65 @@
       size = 10000;
       expireDuplicatesFirst = true;
       path = "${config.xdg.cacheHome}/zsh/history";
+      ignorePatterns = [
+        "rm *"
+        "pkill *"
+      ];
     };
+
+    sessionVariables = { };
 
     shellAliases = {
       nixdev = "nix develop -c zsh";
+    };
+
+    siteFunctions = {
+      myprompt = /* zsh */ ''
+        if [[ -v IN_NIX_SHELL ]]; then
+          PS1="%F{blue}(nix-sh) "
+        else
+          PS1=""
+        fi
+        PS1="%B''${PS1}%(!.%F{red}root.%F{cyan}%n)%F{blue}@%F{cyan}%m%F{blue}:%F{cyan}%3~ %(!.%F{red}#.%F{green}$)%b%f "
+        RPROMPT="%(?..%B%F{red}<FAIL>%b %?)%f"
+      '';
+
+      extract = /* zsh */ ''
+        if [ -z "$1" ]; then
+          echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+        else
+          if [ -f "$1" ]; then
+            case "$1" in
+              *.tar.bz2)   tar xvjf "$1"    ;;
+              *.tar.gz)    tar xvzf "$1"    ;;
+              *.tar.xz)    tar xvJf "$1"    ;;
+              *.lzma)      unlzma "$1"      ;;
+              *.bz2)       bunzip2 "$1"     ;;
+              *.rar)       unrar x -ad "$1" ;;
+              *.gz)        gunzip "$1"      ;;
+              *.tar)       tar xvf "$1"     ;;
+              *.tbz2)      tar xvjf "$1"    ;;
+              *.tgz)       tar xvzf "$1"    ;;
+              *.zip)       unzip "$1"       ;;
+              *.Z)         uncompress "$1"  ;;
+              *.7z)        7z x "$1"        ;;
+              *.xz)        unxz "$1"        ;;
+              *.exe)       cabextract "$1"  ;;
+              *)           echo "extract: '$1' - unknown archive method" ;;
+            esac
+          else
+            echo "$1 - file does not exist"
+          fi
+        fi
+      '';
+
+      foot_title = /* zsh */ ''
+        print -Pn "\e]0;$1\a"
+
+      '';
+      foot_restore_title = /* zsh */ ''
+        print -Pn "\e]0;$TERM\a"
+      '';
     };
 
     plugins = with pkgs; [
@@ -45,101 +106,82 @@
       }
     ];
 
-    initContent = lib.mkOrder 550 ''
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-      zstyle ':completion:*' menu select
-      setopt COMPLETE_ALIASES
-      zmodload zsh/complist
+    initContent =
+      let
+        initExtraFirst = /* zsh */ ''
 
-      # Binds arrows to history search
-      autoload -U up-line-or-beginning-search
-      autoload -U down-line-or-beginning-search
-      zle -N up-line-or-beginning-search
-      zle -N down-line-or-beginning-search
-      bindkey "^[[A" up-line-or-beginning-search
-      bindkey "^[OA" up-line-or-beginning-search
-      bindkey "^[[B" down-line-or-beginning-search
-      bindkey "^[OB" down-line-or-beginning-search
+        '';
 
-      # Binds arrows and j/k to history search in vim mode
-      bindkey -M viins "^[[A" up-line-or-beginning-search
-      bindkey -M viins "^[OA" up-line-or-beginning-search
-      bindkey -M viins "^[[B" down-line-or-beginning-search
-      bindkey -M viins "^[OB" down-line-or-beginning-search
-      bindkey -M vicmd "^[[A" up-line-or-beginning-search
-      bindkey -M vicmd "^[OA" up-line-or-beginning-search
-      bindkey -M vicmd "^[[B" down-line-or-beginning-search
-      bindkey -M vicmd "^[OB" down-line-or-beginning-search
-      bindkey -M vicmd "k" up-line-or-beginning-search
-      bindkey -M vicmd "j" down-line-or-beginning-search
+        initExtraBeforeCompInit = /* zsh */ ''
+          zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+          zstyle ':completion:*' menu select
+          setopt COMPLETE_ALIASES
+          zmodload zsh/complist
+        '';
 
-      # Fix home/end/delete
-      bindkey "^[[H" beginning-of-line
-      bindkey "^[[F" end-of-line
-      bindkey "^[[3~" delete-char
+        initExtra = /* zsh */ ''
+          # Binds arrows to history search
+          autoload -U up-line-or-beginning-search
+          autoload -U down-line-or-beginning-search
+          zle -N up-line-or-beginning-search
+          zle -N down-line-or-beginning-search
+          bindkey "^[[A" up-line-or-beginning-search
+          bindkey "^[OA" up-line-or-beginning-search
+          bindkey "^[[B" down-line-or-beginning-search
+          bindkey "^[OB" down-line-or-beginning-search
 
-      # Vim binds for tab menu
-      bindkey -M menuselect 'h' vi-backward-char
-      bindkey -M menuselect 'l' vi-forward-char
-      bindkey -M menuselect 'j' vi-down-line-or-history
-      bindkey -M menuselect 'k' vi-up-line-or-history
+          # Binds arrows and j/k to history search in vim mode
+          bindkey -M viins "^[[A" up-line-or-beginning-search
+          bindkey -M viins "^[OA" up-line-or-beginning-search
+          bindkey -M viins "^[[B" down-line-or-beginning-search
+          bindkey -M viins "^[OB" down-line-or-beginning-search
+          bindkey -M vicmd "^[[A" up-line-or-beginning-search
+          bindkey -M vicmd "^[OA" up-line-or-beginning-search
+          bindkey -M vicmd "^[[B" down-line-or-beginning-search
+          bindkey -M vicmd "^[OB" down-line-or-beginning-search
+          bindkey -M vicmd "k" up-line-or-beginning-search
+          bindkey -M vicmd "j" down-line-or-beginning-search
 
-      # Prompt
-      autoload -U colors && colors
+          # Fix home/end/delete
+          bindkey "^[[H" beginning-of-line
+          bindkey "^[[F" end-of-line
+          bindkey "^[[3~" delete-char
 
-      prompt() {
-        [[ -v IN_NIX_SHELL ]] && PS1="%F{blue}(nix-sh) " || PS1=""
-        PS1="%B''${PS1}%(!.%F{red}root.%F{cyan}%n)%F{blue}@%F{cyan}%m%F{blue}:%F{cyan}%3~ %(!.%F{red}#.%F{green}$)%b%f "
-        RPROMPT="%(?..%B%F{red}<FAIL>%b %?)%f"
-      }
+          # Vim binds for tab menu
+          bindkey -M menuselect 'h' vi-backward-char
+          bindkey -M menuselect 'l' vi-forward-char
+          bindkey -M menuselect 'j' vi-down-line-or-history
+          bindkey -M menuselect 'k' vi-up-line-or-history
 
-      precmd_functions+=prompt
+          # Prompt
+          autoload -U colors && colors
 
-      case $TERM in
-          foot*)
-              title () {print -Pn "\e]0;$1\a"}
-              preexec_functions+=title
+          autoload -Uz add-zsh-hook myprompt && add-zsh-hook precmd myprompt
 
-              restoretitle () {print -Pn "\e]0;$TERM\a"}
-              precmd_functions+=restoretitle
-          ;;
-      esac
-    '';
+          case $TERM in
+              foot*)
+                  autoload -Uz add-zsh-hook foot_title foot_restore_title
+                  add-zsh-hook preexec foot_title
+                  add-zsh-hook precmd foot_restore_title
+              ;;
+          esac
+        '';
 
-    envExtra = ''
-      # Generic extract function
-      function extract {
-        if [ -z "$1" ]; then
-          echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
-        else
-          if [ -f $1 ]; then
-            case $1 in
-              *.tar.bz2)   tar xvjf $1    ;;
-              *.tar.gz)    tar xvzf $1    ;;
-              *.tar.xz)    tar xvJf $1    ;;
-              *.lzma)      unlzma $1      ;;
-              *.bz2)       bunzip2 $1     ;;
-              *.rar)       unrar x -ad $1 ;;
-              *.gz)        gunzip $1      ;;
-              *.tar)       tar xvf $1     ;;
-              *.tbz2)      tar xvjf $1    ;;
-              *.tgz)       tar xvzf $1    ;;
-              *.zip)       unzip $1       ;;
-              *.Z)         uncompress $1  ;;
-              *.7z)        7z x $1        ;;
-              *.xz)        unxz $1        ;;
-              *.exe)       cabextract $1  ;;
-              *)           echo "extract: '$1' - unknown archive method" ;;
-            esac
-          else
-            echo "$1 - file does not exist"
-          fi
-        fi
-      }
-    '';
+        initExtraAfter = /* zsh */ ''
 
-    # Autostart Hyprland
-    loginExtra = ''
+        '';
+      in
+      lib.mkMerge [
+        (lib.mkBefore initExtraFirst)
+        (lib.mkOrder 550 initExtraBeforeCompInit)
+        initExtra
+        (lib.mkAfter initExtraAfter)
+      ];
+
+    envExtra = /* zsh */ "";
+
+    loginExtra = /* zsh */ ''
+      # Autostart Hyprland
       if which uwsm > /dev/null; then
         if uwsm check may-start > /dev/null; then
           exec uwsm start hyprland_uwsm.desktop
@@ -152,5 +194,9 @@
 
       true
     '';
+
+    logoutExtra = /* zsh */ "";
+
+    profileExtra = /* zsh */ "";
   };
 }
