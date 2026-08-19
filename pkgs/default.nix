6ca69920b9e8ad592a637273be793f74a145d775
rafaelrc7 <contact@rafaelrc.com>
2026-08-18 22:57:08 -0300

    Remove krisp patcher

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,6 @@ let
   cuneifontsPkgs = (pkgs.lib.callPackagesWith pkgs ./cuneifont { });
 in
 {
-  krisp-patcher = pkgs.callPackage ./krisp-patcher { };
   modorganizer2-linux-installer = pkgs.callPackage ./modorganizer2-linux-installer { };
   vim-spell-dict = pkgs.callPackage ./vim-spell { };
   cuneifonts = pkgs.symlinkJoin {
