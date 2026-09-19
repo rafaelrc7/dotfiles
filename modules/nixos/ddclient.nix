c333871e4e4142cb8827c94faaec3f8359ccc260
rafaelrc7 <contact@rafaelrc.com>
2026-09-19 19:37:03 -0300

    Fix ddclient failure on boot

--- modules/nixos/ddclient.nix
+++ modules/nixos/ddclient.nix
@@ -4,4 +4,7 @@
     enable = true;
     configFile = "/etc/ddclient.conf";
   };
+
+  # https://github.com/NixOS/nixpkgs/issues/350408
+  systemd.services.ddclient.after = [ "nss-user-lookup.target" ];
 }
